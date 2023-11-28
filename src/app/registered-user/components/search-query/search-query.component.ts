d50f0b954b6d6775ecf8252d35ce22bded79d9e3
Mr-Rohila <mr.rohilass@gmail.com>
2023-11-28 15:07:11 +0530

    Add utils and search Query service

--- src/app/registered-user/components/search-query/search-query.component.ts
+++ src/app/registered-user/components/search-query/search-query.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchQueryService } from '../../services/search-query.service';
-import { ValidatorService } from 'src/app/Utils/validator.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,43 +9,22 @@ import Swal from 'sweetalert2';
 })
 export class SearchQueryComponent implements OnInit {
 
-  constructor(private _searchQueryService: SearchQueryService, private _validatorService: ValidatorService) { }
+  constructor(private _searchQueryService: SearchQueryService) { }
 
-  queryId = "";
-  queryResponse = {
-    "id": "QRA140920230001",
-    "type": "Payment Refund",
-    "initDate": '12-07-2023',
-    "updateDate": '18-07-2023',
-    "ststus": "Solved",
-    "comment": "Query solved and refund ininitiated from our side. Please wait 3 days for refund",
-  };
-
+  queryResponse: any = [];
+  selectedQuery: any;
   ngOnInit(): void {
   }
 
-  onSubmit() {
-    var isValidate = this._validatorService.nonEmptyString(this.queryId);
-    if (!isValidate) {
-      Swal.fire('', 'Please enter valid Complaint Id', 'warning');
-      return;
-    }
-    this._searchQueryService.searchComplaint(this.queryId).subscribe(
-      {
-        next: (response: any) => {
-          if (response?.length > 0) {
-            this.queryResponse = response[0];
-          }
-          else {
-            Swal.fire('', 'Data not found ', 'info');
-          }
-        },
-        error: error => {
-          console.log(error)
-          Swal.fire('', 'Data not found ', 'info');
-        }
+  loadQuery() {
+    this._searchQueryService.getAllQueries().subscribe({
+      next: (res: any) => {
+        this.queryResponse = res;
+      },
+      error: (err) => {
+        console.log(err);
+        Swal.fire('Faield', 'Something wrong, try after some time', 'error');
       }
-    );
-    return false;
+    })
   }
 }
--- src/app/registered-user/services/search-query.service.ts
+++ src/app/registered-user/services/search-query.service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import API_URls from 'src/app/app-constance/API_URLs';
+import baserUrl from 'src/app/helper/baserUrl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchQueryService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
 
-  searchComplaint(queryId: string) {
-    return this._http.get('https://jsonplaceholder.typicode.com/posts/1');
+  getAllQueries() {
+    return this._http.get(`${baserUrl}` + API_URls.SEARCH_QUERY.GET_ALL_QUERIES);
   }
-  
+
 }
